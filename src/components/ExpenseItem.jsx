88504954759f82fd41aad4d2d247566165e7e1ef
Chris Millington <chris@themadllama.com>
2023-09-03 07:24:09 +0100

    50 - Object destructuring

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,17 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
-  // No Longer needed as all in props
-  // const ExpenseDate = new Date(2021, 2, 28);
-  // const ExpenseTitle = "Manicure Llama";
-  // const ExpenseAmount = 348.99;
-
+// Parameters broken down by Object destructuring
+function ExpenseItem({ date, title, amount }) {
   return (
     <div className="expense-item">
-      <p>{props.date.toISOString()}</p>
+      <p>{date.toISOString()}</p>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">£{props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">£{amount}</div>
       </div>
     </div>
   );
