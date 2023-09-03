6a894f0c1ec5c6fedde90d1221c9f9d6916ddb3e
Chris Millington <chris@themadllama.com>
2023-09-03 07:58:03 +0100

    Expense Date Component Added

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,10 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 // Parameters broken down by Object destructuring
 function ExpenseItem({ date, title, amount }) {
   return (
     <div className="expense-item">
-      <p>{date.toISOString()}</p>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">£{amount}</div>
