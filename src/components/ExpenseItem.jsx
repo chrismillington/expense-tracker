96a47e52e781164e6d1adceb5b2d739e703b795a
Chris Millington <chris@themadllama.com>
2023-09-04 18:46:27 +0100

    Upto 52 - Concept of Composistion

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,17 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "./Card";
 
 // Parameters broken down by Object destructuring
 function ExpenseItem({ date, title, amount }) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">£{amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
